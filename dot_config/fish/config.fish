d413710815dbd8b6e7277fa01daf6979a422af15
d- <d@wood.ac>
2023-04-10 12:29:26 +0000

    remove tmux in fish.conf to avoid recursion

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,7 +6,3 @@ conda activate eq
 export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager
 
 
-if pgrep tmux >/dev/null 2>&1;
-    tmux attach-session -d || tmux new-session | nohup tmux source ~/.config/tmux/.tmux.conf >/dev/null 2>&1;
-end
-
