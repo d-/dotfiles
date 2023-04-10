692e405a16dd105b517d1182aef1860979218c5c
Ubuntu <ubuntu@ip-10-60-20-225.ec2.internal>
2023-04-10 02:46:01 +0000

    attach to tmux automatically

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,5 +3,17 @@
 # !! Contents within this block are managed by 'conda init' !!
 eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 conda activate eq
+
+export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager
+
+
+if pgrep tmux >/dev/null 2>&1; then
+  if [ -z "$TMUX" ]; then
+    # Attach to the last session
+    tmux attach-session -d || tmux new-session
+  fi
+fi
+
+@source ~/.config/tmux/.tmux.conf
 # <<< conda initialize <<<
 
