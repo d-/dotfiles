67650f148873f19eec6fbc39c4ca1c1447041f0f
d- <d@wood.ac>
2023-04-10 03:10:05 +0000

    add fish shell tmux handling

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,19 +1,12 @@
 
 # >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
 eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 conda activate eq
 
 export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager
 
 
-if pgrep tmux >/dev/null 2>&1; then
-  if [ -z "$TMUX" ]; then
-    # Attach to the last session
-    tmux attach-session -d || tmux new-session
-  fi
-fi
-
-@source ~/.config/tmux/.tmux.conf
-# <<< conda initialize <<<
+if pgrep tmux >/dev/null 2>&1;
+    tmux attach-session -d || tmux new-session | nohup tmux source ~/.config/tmux/.tmux.conf >/dev/null 2>&1;
+end
 
