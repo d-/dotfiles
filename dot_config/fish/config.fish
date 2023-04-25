e60222c0793b7e65574baf77491cba4d6381a0d2
d- <d@wood.ac>
2023-04-25 12:18:19 -0400

    add tmux title

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -3,6 +3,6 @@
 eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 conda activate eq
 
-export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager
+export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager:$HOME/bkln-data-service
 
 
