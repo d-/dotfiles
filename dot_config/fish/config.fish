3826018746c016a6b40e12bd5955793039092c0d
d- <d@wood.ac>
2023-05-02 14:41:48 -0400

    add vim binding to fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,6 +1,7 @@
 
 # >>> conda initialize >>>
 eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+fish_vi_key_bindings
 conda activate eq
 
 export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager:$HOME/bkln-data-service
