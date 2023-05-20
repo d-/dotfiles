609fd83ecbfc4391a5eb061bff4a95095be518ae
d- <d@wood.ac>
2023-05-20 10:44:18 -0400

    add indent highlight in vim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -140,9 +140,10 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+    opts = {    
+      space_char_blankline = " ",
+      show_current_context = true,
+      show_current_context_start = true
     },
   },
 
