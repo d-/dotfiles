f2d13894c25d7ed882acf46820fcc12a9bf0503a
David Wood <d-@users.noreply.github.com>
2024-08-30 09:39:13 -0400

    simplify

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,18 +1,5 @@
 
-<<<<<<< HEAD
-# >>> conda initialize >>>
-eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-fish_vi_key_bindings
-conda activate eq
-
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager:$HOME/bkln-data-service
-export LYNX_CFG=~/.config/lynx/.lynxrc
-fzf_configure_bindings --directory=\cf --git_log=\cg --git_status=\cs --variables=\ce
-=======
-eval ~/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 conda activate eq
 
 export PYTHONPATH=$HOME/factset-db:$HOME/strat_manager
 
->>>>>>> 277b5f0 (add kitty)
